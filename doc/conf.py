0334a1b546427fa8d4bc931b0c17372da04cb7c8
aquaticus <1891400+aquaticus@users.noreply.github.com>
2023-04-02 19:29:13 +0200

    Added support for opengraph

--- doc/conf.py
+++ doc/conf.py
@@ -1,7 +1,7 @@
 project = 'Aquaticus Docs'
 copyright = '2023, Aquaticus'
 author = 'aquaticus'
-extensions = ['sphinx_copybutton', 'sphinx.ext.intersphinx']
+extensions = ['sphinx_copybutton', 'sphinx.ext.intersphinx', 'sphinxext.opengraph']
 templates_path = ['_templates']
 exclude_patterns = []
 html_theme = 'sphinx_rtd_theme'
